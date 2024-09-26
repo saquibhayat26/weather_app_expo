df46820e795ed018f60c41a61fb95576a862e303
saquib.md <saquib.md@techurate.com>
2024-09-26 21:57:19 +0530

    layout updated and context provider integrated

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,78 +1,87 @@
-import React, { useEffect, useState } from "react";
 import { Tabs } from "expo-router";
 import { Ionicons } from "@expo/vector-icons";
-import * as Location from "expo-location";
-
-import { useLocationPermission } from "@/hooks/useLocationPermission";
 import { ThemeContext } from "@/context/ThemeContext";
-import { LocationContext } from "@/context/LocationContext";
+import { LocationProvider } from "@/context/LocationContext";
+import { StyleSheet, Animated, View } from "react-native";
+import React from "react";
 
 const TabIcon = ({
   name,
   color,
+  focused,
 }: {
   name: React.ComponentProps<typeof Ionicons>["name"];
   color: string;
+  focused: boolean;
 }) => {
+  const scale = focused ? 1.2 : 1;
   return (
-    <Ionicons
-      name={name}
-      size={28}
-      style={{ marginBottom: -3 }}
-      color={color}
-    />
+    <Animated.View style={{ transform: [{ scale }] }}>
+      <Ionicons
+        name={name}
+        size={28}
+        style={{ marginBottom: -3 }}
+        color={color}
+      />
+    </Animated.View>
   );
 };
 
 export default function TabLayout() {
-  // const [location, setLocation] = useState(null);
-  // const [errorMsg, setErrorMsg] = useState(null);
-  // const { requestLocationPermission } = useLocationPermission();
-
-  // useEffect(() => {
-  //   (async () => {
-  //     const granted = await requestLocationPermission();
-  //     if (!granted) {
-  //       console.log("Location permission denied");
-  //       setErrorMsg("Permission to access location was denied" as any);
-  //       return;
-  //     }
-  //     let location = await Location.getCurrentPositionAsync({});
-  //     setLocation(location as any);
-  //   })();
-  // }, []);
-
   return (
     <ThemeContext.Provider value="light">
-      {/* <LocationContext.Provider value={{ location, errorMsg }}> */}
-      <Tabs
-        screenOptions={{
-          headerShown: false,
-        }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: "Home",
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon name={focused ? "home" : "home-outline"} color={color} />
-            ),
+      <LocationProvider>
+        <Tabs
+          screenOptions={{
+            headerShown: false,
+            tabBarShowLabel: true,
+            tabBarLabelStyle: styles.tabLabelStyle,
+            tabBarStyle: styles.tabBarStyle,
+            tabBarInactiveTintColor: "#aca9a9",
+            tabBarActiveTintColor: "#fff",
           }}
-        />
-        <Tabs.Screen
-          name="explore"
-          options={{
-            title: "Explore",
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcon
-                name={focused ? "code-slash" : "code-slash-outline"}
-                color={color}
-              />
-            ),
-          }}
-        />
-      </Tabs>
-      {/* </LocationContext.Provider> */}
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: "Home",
+              tabBarIcon: ({ color, focused }) => (
+                <TabIcon
+                  name={focused ? "home" : "home-outline"}
+                  color={color}
+                  focused={focused}
+                />
+              ),
+            }}
+          />
+          <Tabs.Screen
+            name="explore"
+            options={{
+              title: "Explore",
+              tabBarIcon: ({ color, focused }) => (
+                <TabIcon
+                  name={focused ? "code-slash" : "code-slash-outline"}
+                  color={color}
+                  focused={focused}
+                />
+              ),
+            }}
+          />
+        </Tabs>
+      </LocationProvider>
     </ThemeContext.Provider>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    backgroundColor: "#000",
+    paddingVertical: 10,
+    height: 60,
+  },
+  tabLabelStyle: {
+    fontSize: 12,
+    fontWeight: "bold",
+    fontFamily: "SpaceMono",
+  },
+});
--- context/LocationContext.tsx
+++ context/LocationContext.tsx
@@ -1,6 +1,50 @@
-import { createContext } from "react";
+import React, { createContext, useEffect, useMemo, useState } from "react";
+import * as Location from "expo-location";
+import { useLocationPermission } from "@/hooks/useLocationPermission";
 
-export const LocationContext = createContext({
+interface LocationContextType {
+  location: Location.LocationObject | null;
+  errorMsg: string | null;
+}
+
+export const LocationContext = createContext<LocationContextType>({
   location: null,
   errorMsg: null,
 });
+
+export const LocationProvider = ({
+  children,
+}: {
+  children: React.ReactNode;
+}) => {
+  const [location, setLocation] = useState<Location.LocationObject | null>(
+    null
+  );
+  const [errorMsg, setErrorMsg] = useState<string | null>(null);
+
+  const { requestLocationPermission } = useLocationPermission();
+
+  useEffect(() => {
+    (async () => {
+      const granted = await requestLocationPermission();
+      if (!granted) {
+        console.log("Location permission denied");
+        setErrorMsg("Permission to access location was denied");
+        return;
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, [requestLocationPermission]);
+
+  const contextValue = useMemo(
+    () => ({ location, errorMsg }),
+    [location, errorMsg]
+  );
+
+  return (
+    <LocationContext.Provider value={contextValue}>
+      {children}
+    </LocationContext.Provider>
+  );
+};
